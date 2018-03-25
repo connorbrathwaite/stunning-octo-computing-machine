58183b11e830fb14407195d921c2af231b32d7c5
Connor Brathwaite <connor.brathwaite@gmail.com>
2018-03-25 15:56:04 -0400

    (feat): SearchList rendering preformatted query results

--- components/SearchList.js
+++ components/SearchList.js
@@ -6,12 +6,12 @@ import {
   ActivityIndicator,
   View
 } from 'react-native'
-import {Query} from 'react-apollo'
 import {gql} from 'apollo-boost'
+import {Query} from 'react-apollo'
+import {ListItem} from 'react-native-elements'
 import * as R from 'ramda'
 
 const isFetchingMore = R.equals(R.always(4))
-
 export const getRepoByOwnerAndName = gql`
   query {
     repository(owner: "facebook", name: "react") {
@@ -30,11 +30,46 @@ export const getRepoByOwnerAndName = gql`
     }
   }
 `
+
+const getReposByName = gql`
+  query($name: String!) {
+    search(query: $name, type: REPOSITORY, first: 10) {
+      repositoryCount
+      nodes {
+        ... on Repository {
+          id
+          name
+          owner {
+            id
+            login
+          }
+        }
+      }
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+    }
+  }
+`
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10
+  }
+})
 export default function SearchList() {
   return (
     <Query
-      query={getRepoByOwnerAndName}
-      fetchPolicy="cache-first"
+      query={getReposByName}
+      variables={{name: 'react'}}
+      notifyOnNetworkStatusChange
     >
       {({
         loading,
@@ -44,13 +79,47 @@ export default function SearchList() {
         fetchMore,
         networkStatus
       }) => {
-        if (loading) return null
+        if (loading)
+          return (
+            <View
+              style={[styles.container, styles.horizontal]}
+            >
+              <ActivityIndicator size="large" />
+            </View>
+          )
+
         if (error) {
-          console.error(error)
-          return null // TODO <Error {...error} />
+          data = require('./mock').default.data
+          // return null // TODO <Error {...error} />
         }
 
-        return <Text>{JSON.stringify(data)}</Text>
+        const {
+          nodes,
+          repositoryCount,
+          pageInfo
+        } = data.search
+        return (
+          <FlatList
+            data={nodes}
+            keyExtractor={R.prop('id')}
+            refreshing={isFetchingMore(networkStatus)}
+            onRefresh={refetch}
+            onEndReachedThreshold={0.5}
+            ListEmptyComponent={() => (
+              <Text>No repositories found :'(</Text>
+            )}
+            ListHeaderComponent={() => (
+              <Text>
+                {repositoryCount} repositories found
+              </Text>
+            )}
+            renderItem={({item}) =>
+              item ? (
+                <ListItem key={item.id} title={item.name} />
+              ) : null
+            }
+          />
+        )
       }}
     </Query>
   )
---
+++ components/mock.js
@@ -0,0 +1,93 @@
+export default {
+  "data": {
+    "search": {
+      "pageInfo": {
+        "hasNextPage": true,
+        "endCursor": "Y3Vyc29yOjEw"
+      },
+      "repositoryCount": 447542,
+      "nodes": [
+        {
+          "id": "MDEwOlJlcG9zaXRvcnkxMDI3MDI1MA==",
+          "name": "react",
+          "owner": {
+            "id": "MDEyOk9yZ2FuaXphdGlvbjY5NjMx",
+            "login": "facebook"
+          }
+        },
+        {
+          "id": "MDEwOlJlcG9zaXRvcnk0Mjg3OTIx",
+          "name": "react",
+          "owner": {
+            "id": "MDEyOk9yZ2FuaXphdGlvbjE2OTY4NjY=",
+            "login": "reactphp"
+          }
+        },
+        {
+          "id": "MDEwOlJlcG9zaXRvcnk3NTM5NjU3NQ==",
+          "name": "react",
+          "owner": {
+            "id": "MDQ6VXNlcjMyNDk2NTM=",
+            "login": "duxianwei520"
+          }
+        },
+        {
+          "id": "MDEwOlJlcG9zaXRvcnk5MDc1OTkzMA==",
+          "name": "react",
+          "owner": {
+            "id": "MDQ6VXNlcjQ1MDcxMDE=",
+            "login": "discountry"
+          }
+        },
+        {
+          "id": "MDEwOlJlcG9zaXRvcnk3MjYyODI4NQ==",
+          "name": "react",
+          "owner": {
+            "id": "MDQ6VXNlcjIwNjUzNjQz",
+            "login": "Cathy0807"
+          }
+        },
+        {
+          "id": "MDEwOlJlcG9zaXRvcnk3MzAzOTE2MA==",
+          "name": "react-pxq",
+          "owner": {
+            "id": "MDQ6VXNlcjIwMjk3MjI3",
+            "login": "bailicangdu"
+          }
+        },
+        {
+          "id": "MDEwOlJlcG9zaXRvcnk3NzUxMzQxOQ==",
+          "name": "react",
+          "owner": {
+            "id": "MDEyOk9yZ2FuaXphdGlvbjI0ODA1MTQy",
+            "login": "react-redux-antd-es6"
+          }
+        },
+        {
+          "id": "MDEwOlJlcG9zaXRvcnkzOTMxNjUzNQ==",
+          "name": "react",
+          "owner": {
+            "id": "MDQ6VXNlcjExMzczMTQ=",
+            "login": "azat-co"
+          }
+        },
+        {
+          "id": "MDEwOlJlcG9zaXRvcnk0NTc1MzA3OQ==",
+          "name": "react",
+          "owner": {
+            "id": "MDEyOk9yZ2FuaXphdGlvbjMxMTY5MjA=",
+            "login": "werein"
+          }
+        },
+        {
+          "id": "MDEwOlJlcG9zaXRvcnk2MTU3MzcwMg==",
+          "name": "react",
+          "owner": {
+            "id": "MDEyOk9yZ2FuaXphdGlvbjE2ODU2NzM1",
+            "login": "ui-router"
+          }
+        }
+      ]
+    }
+  }
+}
